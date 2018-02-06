181b243bf880fbfa40398d887c61ecad1a7aab8c
Sukhman Kaur <Sukhman@sukhmans-macbook-pro.local>
2018-02-06 11:12:24 -0600

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Sukhman Kaur on 2/6/18.
+//  Copyright © 2018 Jaskaran Singh. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
